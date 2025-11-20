d9f62e606148a69f308d4c4c32aa723c778654c0
dalier <1002920232@unajma.edu.pe>
2025-11-19 19:30:27 -0500

    Ignore environments folder

--- src/environments/environments.ts
+++
@@ -1,9 +0,0 @@
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAPgXG-UMdkt4b-Y0V1P5JOoWBL0xZWHKc",
-  authDomain: "control-de-asistencia-41bb2.firebaseapp.com",
-  projectId: "control-de-asistencia-41bb2",
-  storageBucket: "control-de-asistencia-41bb2.firebasestorage.app",
-  messagingSenderId: "119048846960",
-  appId: "1:119048846960:web:6cf06438bb6c6aaf73feb5"
-};
