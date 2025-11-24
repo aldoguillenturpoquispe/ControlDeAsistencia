0ee038c45f8f9bf0f07a5ddeec2a7726239d5ac5
dalier <1002920232@unajma.edu.pe>
2025-11-24 10:14:07 -0500

    header agregado cerrar sesion y configuracion

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  standalone: true,
+  imports: [CommonModule, RouterLinkActive, RouterLink],
   templateUrl: './header.html',
-  styleUrl: './header.css',
+  styleUrl: './header.css'
 })
 export class Header {
-  menuOpen = false;
-    toggleMenu() {
-    this.menuOpen = !this.menuOpen;
+  showUserMenu = false;
+
+  constructor(private router: Router) {}
+
+  toggleUserMenu() {
+    this.showUserMenu = !this.showUserMenu;
+  }
+
+  configuracion() {
+    console.log('Ir a configuración');
+    this.showUserMenu = false;
+    // this.router.navigate(['/configuracion']);
+  }
+
+  cerrarSesion() {
+    console.log('Cerrando sesión...');
+    this.showUserMenu = false;
+    alert('Sesión cerrada');
+    this.router.navigate(['/login']);
   }
 }
