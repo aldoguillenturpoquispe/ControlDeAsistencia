722a868eaf07645ca6b177804f1bb0e1b46e87a7
dalier <1002920232@unajma.edu.pe>
2025-11-24 11:20:03 -0500

    reportes con solo lo necesario

--- src/app/components/estadisticas/estadisticas.ts
+++ src/app/components/estadisticas/estadisticas.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { StatsCharts } from './stats-charts/stats-charts';
 import { StatsResumenEstados } from './stats-resumen-estados/stats-resumen-estados';
 import { StatsTopUsuarios } from './stats-top-usuarios/stats-top-usuarios';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-estadisticas',
